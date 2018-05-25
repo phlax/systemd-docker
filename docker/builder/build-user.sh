bfec4481434dd4c74e621a51a1917fe37d31ce2d
Ryan Northey <ryan@synca.io>
2018-05-25 18:10:12 +0100

    Cleanup build recipe

--- docker/builder/build-user.sh
+++ docker/builder/build-user.sh
@@ -4,16 +4,9 @@ set -e
 
 APP_DIR=$(readlink -f --canonicalize $(eval echo $APP_DIR))
 
-
-echo "Installing package: $APP_PKG into $APP_DIR"
 cd $APP_DIR
 virtualenv -p /usr/bin/python3 .
 . bin/activate \
-    && pip install \
-           -q \
-           --no-cache-dir \
-           --process-dependency-links \
-           $APP_PKG \
     && cd src \
     && git clone https://github.com/phlax/ctrl.core \
     && git clone https://github.com/phlax/ctrl.config \
